9b72a59b538478cd6f62cdfd6a5109ac7ed045d9
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-20 15:58:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,16 @@
+require 'pry'
 # Your code goes here!
 class Anagram
   
   attr_accessor :word
   
+  binding.pry
   def initialize(word)
     @word = word
   end
   
+  def self.match
+    
   
 end
   
